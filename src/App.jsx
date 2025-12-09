bb6a0fc5fcaee6f0cceaf963d56932acef331d01
ebenamoafo <ebenamoafo222@gmail.com>
2025-12-09 18:05:37 +0000

    feat: add job creation and detail viewing functionality - Implemented AddJobPage for job submission with form handling. - Enhanced JobPage to display job details and include delete functionality. - Integrated jobLoader for fetching job data based on ID.

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,16 @@ import MainLayout from './layouts/MainLayouts';
 import JobsPage from './pages/JobsPage';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} />
+      <Route path="/add-job" element={<AddJobPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>,
   ),
